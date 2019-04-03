05ee2ce73aed8aa3d80006a45294f803d5075494
mjaman <mjaman@mit.edu>
2019-04-03 13:31:01 -0400

    Update preconditioners.py

--- petram/helper/preconditioners.py
+++ petram/helper/preconditioners.py
@@ -431,6 +431,34 @@ def gmres(atol=1e-24, rtol=1e-12, max_num_iter=5,
         # keep this object from being freed...
         gmres._prc = preconditioner
     return gmres
+
+@prc.block
+def fgmres(atol=1e-24, rtol=1e-12, max_num_iter=5,
+          kdim=50, print_level=-1, preconditioner=None, **kwargs):
+    prc = kwargs.pop('prc')
+    blockname = kwargs.pop('blockname')
+
+    if use_parallel:
+        fgmres = mfem.FGMRESSolver(MPI.COMM_WORLD)
+    else:
+        fgmres = mfem.FGMRESSolver()
+    fgmres.iterative_mode = False
+    fgmres.SetRelTol(rtol)
+    fgmres.SetAbsTol(atol)
+    fgmres.SetMaxIter(max_num_iter)
+    fgmres.SetPrintLevel(print_level)    
+    fgmres.SetKDim(kdim)
+    r0 = prc.get_row_by_name(blockname)
+    c0 = prc.get_col_by_name(blockname)
+    
+    A0 = prc.get_operator_block(r0, c0)    
+
+    fgmres.SetOperator(A0)
+    if preconditioner is not None:
+        fgmres.SetPreconditioner(preconditioner)
+        # keep this object from being freed...
+        fgmres._prc = preconditioner
+    return fgmres
  
 @prc.block
 def pcg(atol=1e-24, rtol=1e-12, max_num_iter=5,
