11de3e801ac6c2cd248fb289aec2d1f61f3d8336
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2019-05-13 20:16:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='PetraM_Base',
-    version='0.6.0',
+    version='0.7.0',
 
     description='PetraM base package',
     long_description=long_description,
