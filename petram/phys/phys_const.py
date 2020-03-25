9a7d0b9c4593df6f4c99f7bae43b59432e9a511b
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-03-25 15:34:06 -0400

    added Boltzmann constant(k_B)

--- petram/phys/phys_const.py
+++ petram/phys/phys_const.py
@@ -7,3 +7,4 @@ epsilon0 = 8.8541878176e-12       # vacuum permittivity
 mu0      = 4* np.pi*1e-7          # vacuum permiability
 c        = 2.99792458e8           # speed of light
 q0       = 1.60217662e-19         # electron charge
+k_B      = 1.380649×10−23         # Boltzmann Constant (J/K)
