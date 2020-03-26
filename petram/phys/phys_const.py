d41255db85314aa35db28b9635e75dbbe2555db2
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-03-26 09:50:17 -0400

    added Boltzmann constant(k_B)

--- petram/phys/phys_const.py
+++ petram/phys/phys_const.py
@@ -5,6 +5,8 @@ import numpy as np
 
 epsilon0 = 8.8541878176e-12       # vacuum permittivity
 mu0      = 4* np.pi*1e-7          # vacuum permiability
-c        = 2.99792458e8           # speed of light
+c        = 2.99792458e8           # speed of light (m/s)
 q0       = 1.60217662e-19         # electron charge
-k_B      = 1.380649×10−23         # Boltzmann Constant (J/K)
+k_B      = 1.380649e-23     # Boltzmann Constant (J/K)
+alpha_fine  = 0.0072973525693     # fine structure constant
+Da       = 1.66053906660e-27      # atomic mass unit (u or Dalton) (kg)
