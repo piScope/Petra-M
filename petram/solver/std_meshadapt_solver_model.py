5319b04ae1971edd6c320f0b75ec2d3e00aed35b
Morteza H. Siboni <hakimi1364@gmail.com>
2020-07-17 17:41:23 -0400

    Exposes a few other mesh adapt options to the user

--- petram/solver/std_meshadapt_solver_model.py
+++ petram/solver/std_meshadapt_solver_model.py
@@ -17,157 +17,45 @@ rprint = debug.regular_print('StdMeshAdaptSolver')
 
 from petram.solver.std_solver_model import StdSolver, StandardSolver
 
-def get_field_z_averaged(pumi_mesh, field_name, field_type, grid):
-  # get the mfem mesh and fespace
-  fes = grid.ParFESpace()
-  mesh = fes.GetParMesh()
+def get_field_component(pumi_mesh, in_field, component):
+  name = ""
+  if component == 0:
+    name = "emag"
+  elif component == 1:
+    name = "ex"
+  elif component == 2:
+    name = "ey"
+  elif component == 3:
+    name = "ez"
+  else:
+    assert False, "wrong component: " + component
 
-  # we expect the pumi mesh to have a numbering with the name
-  # "local_vert_numbering"
-  numbering = pumi_mesh.findNumbering("local_vert_numbering")
-  if numbering == None:
-    assert False, "numbering \"local_vert_numbering\" was not found"
-
-
-  # create the necessary fields
-  field_z = pyCore.createFieldOn(pumi_mesh, field_name, field_type)
-  dim = pumi_mesh.getDimension()
-  count_field = pyCore.createFieldOn(pumi_mesh, "count_field", pyCore.SCALAR)
-  sol_field = pyCore.createFieldOn(pumi_mesh, "sol_field_averaged", pyCore.VECTOR)
-
-  # initialize the count and sol field to zero
+  field_component = pyCore.createFieldOn(pumi_mesh,
+					name,
+					pyCore.SCALAR)
   it = pumi_mesh.begin(0)
   while True:
     ent = pumi_mesh.iterate(it)
     if not ent:
       break
-    pyCore.setScalar(count_field, ent, 0, 0.0)
-    p = pyCore.Vector3(0.0, 0.0, 0.0)
-    pyCore.setVector(sol_field, ent, 0, p)
+    vec = pyCore.Vector3()
+    pyCore.getVector(in_field, ent, 0, vec)
+    if component == 0:
+      outvalue = vec.x() * vec.x() + vec.y() * vec.y() + vec.z() * vec.z()
+      outvalue = math.sqrt(outvalue)
+    elif component == 1:
+      outvalue = vec.x()
+    elif component == 2:
+      outvalue = vec.y()
+    elif component == 3:
+      outvalue = vec.z()
+    else:
+      assert False, ""
+    pyCore.setScalar(field_component, ent, 0, outvalue)
   pumi_mesh.end(it)
 
-  it = pumi_mesh.begin(dim)
-  eid = 0
-  while True:
-    ent = pumi_mesh.iterate(it)
-    if not ent:
-      break
+  return field_component
 
-    felem = fes.GetFE(eid)
-    elem_vert = mfem.geom.Geometry().GetVertices(felem.GetGeomType())
-
-    vval = DenseMatrix()
-    pmat = DenseMatrix()
-    grid.GetVectorValues(eid, elem_vert, vval, pmat)
-    pyCore.PCU_ALWAYS_ASSERT(vval.Width() == 4)
-    pyCore.PCU_ALWAYS_ASSERT(vval.Height() == 3)
-
-    mfem_vids = mesh.GetElementVertices(eid)
-
-    for i in range(4):
-      current_count = pumi_mesh.getVertScalarField(count_field, ent, i, 0)
-      current_sol = pumi_mesh.getVertVectorField(sol_field, ent, i, 0)
-      pumi_vid = pumi_mesh.getVertNumbering(numbering, ent, i, 0, 0)
-      # this is the index of pumi_vid in mfem_vids list
-      # (note that we need to do this because of ReorientTet call)
-      j = mfem_vids.index(pumi_vid)
-      pumi_mesh.setVertScalarField(count_field, ent, i, 0, current_count + 1.0)
-      # pumi_mesh.setVertVectorField(sol_field, ent, i, 0, current_sol.x()+vval[0,j],
-      #                                                    current_sol.y()+vval[1,j],
-      #                                                    current_sol.z()+vval[2,j])
-      pumi_mesh.setVertVectorField(sol_field, ent, i, 0, vval[0,j],
-                                                         vval[1,j],
-                                                         vval[2,j])
-
-    eid = eid + 1
-
-  pumi_mesh.end(it)
-
-  # do the average since each vertex gets a value from multiple tets
-  it = pumi_mesh.begin(0)
-  while True:
-    ent = pumi_mesh.iterate(it)
-    if not ent:
-      break
-    current_count = pyCore.getScalar(count_field, ent, 0)
-    current_sol = pyCore.Vector3()
-    pyCore.getVector(sol_field, ent, 0, current_sol)
-    # avg_sol = pyCore.Vector3(current_sol.x() / current_count,
-    #                          current_sol.y() / current_count,
-    #                          current_sol.z() / current_count)
-    sol_limit = 5000.0
-    avg_sol_x = sol_limit
-    avg_sol_y = sol_limit
-    avg_sol_z = sol_limit
-    if abs(current_sol.x()) < sol_limit:
-      avg_sol_x = current_sol.x()
-    if abs(current_sol.y()) < sol_limit:
-      avg_sol_y = current_sol.y()
-    if abs(current_sol.z()) < sol_limit:
-      avg_sol_z = current_sol.z()
-    avg_sol = pyCore.Vector3(avg_sol_x,
-                             avg_sol_y,
-                             avg_sol_z)
-    # mag = sqrt(avg_sol.x() * avg_sol.x() +
-    #          avg_sol.y() * avg_sol.y() +
-    #          avg_sol.z() * avg_sol.z())
-    mag = math.sqrt(avg_sol.x() * avg_sol.x()+
-    	            avg_sol.y() * avg_sol.y()+
-    	            avg_sol.z() * avg_sol.z())
-    pyCore.setScalar(field_z, ent, 0, mag)
-    pyCore.setVector(sol_field, ent, 0, avg_sol)
-
-
-
-  pumi_mesh.end(it)
-  pumi_mesh.removeField(count_field)
-  pyCore.destroyField(count_field)
-  # pumi_mesh.removeField(sol_field)
-  # pyCore.destroyField(sol_field)
-  return field_z
-
-def get_curl_ip_field(pumi_mesh, field_name, field_type, field_order, grid):
-  # get the mfem mesh and fespace
-  fes = grid.ParFESpace()
-  mesh = fes.GetParMesh()
-
-
-  # create the necessary fields
-  curl_field = pyCore.createIPField(pumi_mesh, field_name, field_type, field_order)
-  # curl_field_shape = pyCore.getShape(curl_field)
-
-  dim = pumi_mesh.getDimension()
-
-  it = pumi_mesh.begin(dim)
-  eid = 0
-  while True:
-    ent = pumi_mesh.iterate(it)
-    if not ent:
-      break
-
-    elem_transformation = fes.GetElementTransformation(eid)
-    curl_vec = mfem.Vector()
-    grid.GetCurl(elem_transformation, curl_vec)
-
-    # for i in range(4):
-    #   current_count = pumi_mesh.getVertScalarField(count_field, ent, i, 0)
-    #   current_sol = pumi_mesh.getVertVectorField(sol_field, ent, i, 0)
-    #   pumi_vid = pumi_mesh.getVertNumbering(numbering, ent, i, 0, 0)
-    #   # this is the index of pumi_vid in mfem_vids list
-    #   # (note that we need to do this because of ReorientTet call)
-    #   j = mfem_vids.index(pumi_vid)
-    #   pumi_mesh.setVertScalarField(count_field, ent, i, 0, current_count + 1.0)
-    #   pumi_mesh.setVertVectorField(sol_field, ent, i, 0, current_sol.x()+vval[0,j],
-    #                                                      current_sol.y()+vval[1,j],
-    #                                                      current_sol.z()+vval[2,j])
-    # pyCore.setComponents(curl_field, ent, 0, curl_vec.GetData())
-    pumi_mesh.setIPxyz(curl_field, ent, 0, curl_vec[0], curl_vec[1], curl_vec[2])
-
-    eid = eid + 1
-
-  pumi_mesh.end(it)
-
-  return curl_field
 
 def limit_refine_level(pumi_mesh, sizefield, level):
   it = pumi_mesh.begin(0)
@@ -198,25 +86,28 @@ class StdMeshAdaptSolver(StdSolver):
                  self.save_parmesh,  3, {"text":"save parallel mesh"}],
                 [None,
                  self.use_profiler,  3, {"text":"use profiler"}],
-                ["indicator",   self.mesh_adapt_indicator,  0, {}],
-                ["#mesh adapt",   self.mesh_adapt_num,  0, {},],]                
+                ["indicator",   self.mesh_adapt_indicator,  0, {},],
+                ["#mesh adapt",   self.mesh_adapt_num,  0, {},],
+                ["adapt ratio",   self.mesh_adapt_ar,  0, {},]]
 
     def attribute_set(self, v):
         super(StdMeshAdaptSolver, self).attribute_set(v)
-        v["mesh_adapt_indicator"] = ""
+        v["mesh_adapt_indicator"] = "E"
         v["mesh_adapt_num"] = 0
+        v["mesh_adapt_ar"] = 0.1
         return v
                  
     def get_panel1_value(self):
         return (#self.init_setting,
                 self.phys_model,
-                self.init_only, 
+                self.init_only,
                 self.clear_wdir,
                 self.assemble_real,
                 self.save_parmesh,
                 self.use_profiler,
                 self.mesh_adapt_indicator,
-                self.mesh_adapt_num)
+                self.mesh_adapt_num,
+                self.mesh_adapt_ar)
     
     def import_panel1_value(self, v):
         #self.init_setting = str(v[0])        
@@ -227,8 +118,9 @@ class StdMeshAdaptSolver(StdSolver):
         self.save_parmesh = v[4]
         self.use_profiler = v[5]
         self.mesh_adapt_indicator = v[6]
-        self.mesh_adapt_num = int(v[7])
-                 
+        self.mesh_adapt_num = v[7]
+        self.mesh_adapt_ar = v[8]
+
     @debug.use_profiler
     def run(self, engine, is_first = True, return_instance=False):
         dprint1("Entering run", is_first, self.fullpath())
@@ -259,7 +151,7 @@ class StdMeshAdaptSolver(StdSolver):
             instance.solve()
 
         adapt_loop_no = 0;
-        while adapt_loop_no < 3:
+        while adapt_loop_no < self.mesh_adapt_num:
             instance.save_solution(ksol = 0,
                                 skip_mesh = False,
                                 mesh_only = False,
@@ -287,20 +179,38 @@ class StdMeshAdaptSolver(StdSolver):
                                                             pyCore.getLagrange(1))
 
             par_pumi_mesh.NedelecFieldMFEMtoPUMI(pumi_mesh, x, pumi_nedelec_field)
-            # pyCore.writeNedelecVtkFiles("disc_e_field_vis", pumi_mesh)
             pyCore.projectNedelecField(pumi_projected_nedelec_field, pumi_nedelec_field)
-            pyCore.writeASCIIVtkFiles('e_and_projected_e', pumi_mesh)
-            pumi_mesh.writeNative("e_and_projected_e.smb")
+
+            # native file output for debug
+            native_name = "pumi_mesh_e_field_ma_iter_" + str(adapt_loop_no) + ".smb"
+            print("native_name is ", native_name)
+            pumi_mesh.writeNative(native_name)
+
+
             pumi_mesh.removeField(pumi_nedelec_field)
             pyCore.destroyField(pumi_nedelec_field)
-            pyCore.writeASCIIVtkFiles('cont_e_field_vis', pumi_mesh)
 
 
-            ip_field = pyCore.getGradIPField(pumi_projected_nedelec_field, "mfem_grad_ip", 2)
+            print("user choose the indicator ", self.mesh_adapt_indicator)
+            indicator_field = 0;
+            if self.mesh_adapt_indicator == "E":
+              indicator_field = pumi_projected_nedelec_field
+            elif self.mesh_adapt_indicator == "Emag":
+              indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 0)
+            elif self.mesh_adapt_indicator == "Ex":
+              indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 1)
+            elif self.mesh_adapt_indicator == "Ey":
+              indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 2)
+            elif self.mesh_adapt_indicator == "Ez":
+              indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 3)
+            else:
+              assert False, "wrong indicator selected"
+
+            ip_field = pyCore.getGradIPField(indicator_field, "mfem_grad_ip", 2)
 
             ## transferring the curl of E and using it in spr
-            # ip_field = get_curl_ip_field(pumi_mesh, "curl_ip_field", pyCore.VECTOR, 1, x)
-            size_field = pyCore.getTargetSPRSizeField(ip_field, 5000, 0.1, 1.)
+            size_field = pyCore.getSPRSizeField(ip_field, float(self.mesh_adapt_ar))
+
 
             limit_refine_level(pumi_mesh, size_field, 5)
             before_prefix = "before_adapt_"+str(adapt_loop_no);
@@ -309,28 +219,28 @@ class StdMeshAdaptSolver(StdSolver):
 
             pumi_mesh.removeField(ip_field)
             pyCore.destroyField(ip_field)
-            # pyCore.destroyNumbering(pumi_mesh.findNumbering("local_vert_numbering"))
 
             adapt_input = pyCore.configure(pumi_mesh, size_field)
             adapt_input.shouldFixShape = True
             adapt_input.shouldCoarsen = True
             adapt_input.maximumIterations = 6
-            adapt_input.goodQuality = 0.027
+            adapt_input.goodQuality = 0.35 * 0.35 * 0.35 # mean-ratio cubed
 
             pyCore.adaptVerbose(adapt_input)
 
-
             after_prefix = "after_adapt_"+str(adapt_loop_no);
             pyCore.writeASCIIVtkFiles(after_prefix, pumi_mesh);
-            # pumi_mesh.writeNative("adapted_mesh.smb")
+
             # clean up rest of the fields
             pumi_mesh.removeField(pumi_projected_nedelec_field)
             pyCore.destroyField(pumi_projected_nedelec_field)
             pumi_mesh.removeField(size_field)
             pyCore.destroyField(size_field)
+            pumi_mesh.removeField(indicator_field)
+            pyCore.destroyField(indicator_field)
 
             adapted_mesh = mfem.ParMesh(pyCore.PCU_Get_Comm(), pumi_mesh)
-            # add the boundary attributes 
+            # add the boundary attributes
             dim = pumi_mesh.getDimension()
             it = pumi_mesh.begin(dim-1)
             bdr_cnt = 0
