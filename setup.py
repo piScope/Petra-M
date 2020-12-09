93b2c583106154b99dea1d112bdc49e93a4e4e56
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-12-09 08:43:06 -0500

    minor fix

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='PetraM_Base',
 
-    version='1.3.9',
+    version='1.4.0',
 
     description='PetraM base package',
     long_description=long_description,
