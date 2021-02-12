83560874c11eb009a3fd00f85b111be8c6fa5ef9
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2021-02-12 12:39:31 -0500

    fixing c/s

--- petram/sol/integral_evaluator.py
+++ petram/sol/integral_evaluator.py
@@ -84,9 +84,8 @@ def do_integration(expr, solvars, phys, mesh, kind, attrs,
     if not np.isfinite(ans):
         print("not finite", ans)
         from mfem.common.chypre import LF2PyVec, PyVec2PyMat, Array2PyVec, IdentityPyMat
-        print(list(b.GetDataArray()))
-        v2 = LF2PyVec(b, None, horizontal=True)        
-        print(b[0])
+        print(list(gf.GetDataArray())
+        print(list(b.GetDataArray()))        
     return ans
 
 class IntegralEvaluator(EvaluatorAgent):
