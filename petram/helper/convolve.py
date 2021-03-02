0d9668ab5aca083fed343cb6b79535effcebfd63
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2021-03-02 17:24:35 -0500

    checking...

--- petram/helper/convolve.py
+++ petram/helper/convolve.py
@@ -510,14 +510,11 @@ def convolve2d(fes1, fes2, kernel=delta, support=None,
                 vdofs1_all.append(subvdofs2)
             else:
                 vdofs1_all.append(local_vdofs)
-        dprint1("here", myid, len(i2_onenode))
-        if myid == 0:
-            pr = profile_start()
+
+        #if myid == 0:
+        #    pr = profile_start()
 
         for i, x2s, su in zip(i2_onenode, x2_onenode, s_onenode): # loop over fes2
-            if i%10 == 0:
-                dprint1("here2", myid, i, su)
-            
             nd2 = len(x2s)
             #nicePrint("x2s", i, x2s.shape, x2s)
             elmats = []
@@ -602,12 +599,11 @@ def convolve2d(fes1, fes2, kernel=delta, support=None,
                 if has_contribution:
                     elmats.append((j, elmat))
                     
-        if myid == 0:
-            pr.dump_stats("/home/shiraiwa/test.prf")                
-            profile_stop(pr)
-            assert False, "hoge"
-            pr = profile_start()
-                       
+        #if myid == 0:
+        #    pr.dump_stats("/home/shiraiwa/test.prf")                
+        #    profile_stop(pr)
+        #    assert False, "hoge"
+        #    pr = profile_start()
 
                 #print(elmats)
         if len(elmats) > 0:
