3242715e14037f3cebbce0735e8e8d3adcfaa8a0
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-06-28 08:48:37 -0400

    make a condition for damping reduction stronger(try 3)

--- petram/solver/nl_solver_model.py
+++ petram/solver/nl_solver_model.py
@@ -642,7 +642,8 @@ class NewtonSolver(NonlinearBaseSolver):
             elif self._fixed_damping:
                 pass
 
-            elif (err > self._err_guidance*1.05 or err > self._err_before*1.05):
+            #elif (err > self._err_guidance*1.05 or err > self._err_before*1.05):
+            elif err > self._err_before*self.dwidth1):
                 # self.set_damping(self.damping*0.8)
                 # self.set_damping(self.damping*0.7)
                 self.set_damping(self.damping*self.dwidth2)
