31ba52ae997b17c6951c385db1dd8a78a7ee1bb6
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-06-29 19:24:07 -0400

    (WIP) checking the residual

--- petram/solver/nl_solver_model.py
+++ petram/solver/nl_solver_model.py
@@ -724,7 +724,7 @@ class NewtonSolver(NonlinearBaseSolver):
 
         self.residual_record.append(residual)
 
-        if self.kiter > 5 and residual < 3e-7:
+        if self.kiter > 5 and residual < 3e-6:
             self._done = True
 
         if not self._converged and not self._done:
