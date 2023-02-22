143fb141446aa4626a9d0f6d9c33ce5c1208a340
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2023-02-22 10:31:50 -0500

    added a mechanism to build a list of installed constraints

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='PetraM_Base',
 
-    version='2.0.9',
+    version='2.1.0',
 
     description='PetraM base package',
     long_description=long_description,
