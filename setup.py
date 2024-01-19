de8cb4c8ce444645999fc6681f2276456814ac39
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2024-01-19 13:36:06 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='PetraM_Base',
 
-    version='2.1.25',
+    version='2.1.26',
 
     description='PetraM base package',
     long_description=long_description,
