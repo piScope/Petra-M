68c9fc8cdf809ef5b16282009301f60fb438cd1a
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2024-04-25 09:56:40 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='PetraM_Base',
 
-    version='2.1.36',
+    version='2.1.37',
 
     description='PetraM base package',
     long_description=long_description,
