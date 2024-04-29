8b17b9594de8280c77f8bb180cc0dacb42ad642d
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2024-04-29 16:46:35 -0400

    added Levi-Civita in PhysConst

--- petram/phys/phys_const.py
+++ petram/phys/phys_const.py
@@ -32,34 +32,34 @@ levi_civita3[2, 1, 0] = -1
 levi_civita3[0, 2, 1] = -1
 
 levi_civita4 = np.zeros((4, 4, 4, 4), dtype=np.float64)
-levi_civita3[0, 1, 2, 3] = 1
-levi_civita3[3, 0, 1, 2] = -1
-levi_civita3[2, 3, 0, 1] = 1
-levi_civita3[1, 2, 3, 0] = -1
+levi_civita4[0, 1, 2, 3] = 1
+levi_civita4[3, 0, 1, 2] = -1
+levi_civita4[2, 3, 0, 1] = 1
+levi_civita4[1, 2, 3, 0] = -1
 
-levi_civita3[0, 1, 3, 2] = -1
-levi_civita3[2, 0, 1, 3] = 1
-levi_civita3[3, 2, 0, 1] = -1
-levi_civita3[1, 3, 2, 0] = 1
+levi_civita4[0, 1, 3, 2] = -1
+levi_civita4[2, 0, 1, 3] = 1
+levi_civita4[3, 2, 0, 1] = -1
+levi_civita4[1, 3, 2, 0] = 1
 
-levi_civita3[0, 2, 1, 3] = -1
-levi_civita3[3, 0, 2, 1] = 1
-levi_civita3[1, 3, 0, 2] = -1
-levi_civita3[2, 1, 3, 0] = 1
+levi_civita4[0, 2, 1, 3] = -1
+levi_civita4[3, 0, 2, 1] = 1
+levi_civita4[1, 3, 0, 2] = -1
+levi_civita4[2, 1, 3, 0] = 1
 
-levi_civita3[0, 2, 3, 1] = 1
-levi_civita3[1, 0, 2, 3] = -1
-levi_civita3[3, 1, 0, 2] = 1
-levi_civita3[2, 3, 1, 0] = -1
+levi_civita4[0, 2, 3, 1] = 1
+levi_civita4[1, 0, 2, 3] = -1
+levi_civita4[3, 1, 0, 2] = 1
+levi_civita4[2, 3, 1, 0] = -1
 
-levi_civita3[0, 3, 1, 2] = 1
-levi_civita3[2, 0, 3, 1] = -1
-levi_civita3[1, 2, 0, 3] = 1
-levi_civita3[3, 1, 2, 0] = -1
+levi_civita4[0, 3, 1, 2] = 1
+levi_civita4[2, 0, 3, 1] = -1
+levi_civita4[1, 2, 0, 3] = 1
+levi_civita4[3, 1, 2, 0] = -1
 
-levi_civita3[0, 3, 2, 1] = -1
-levi_civita3[1, 0, 3, 2] = 1
-levi_civita3[2, 1, 0, 3] = -1
-levi_civita3[3, 2, 1, 0] = 1
+levi_civita4[0, 3, 2, 1] = -1
+levi_civita4[1, 0, 3, 2] = 1
+levi_civita4[2, 1, 0, 3] = -1
+levi_civita4[3, 2, 1, 0] = 1
 
 
