addubg strong curl-curl. Need to fix directional derivative

petram/helper/integrators/pyvector_curl_integrator.py
@@ -119,9 +119,9 @@ class PyVectorCurlIntegrator(PyVectorCurlIntegratorBase):
 
         tr_merged_arr = np.zeros((tr_nd, self.esdim), dtype=np.complex128)
 
-        scalar_coeff = isinstance(self.lam_real, mfem.Coefficient)
-        if scalar_coeff:
-            assert self.vdim_te == self.vdim_tr, "scalar coefficeint allows only for square matrix"
+        #scalar_coeff = isinstance(self.lam_real, mfem.Coefficient)
+        # if scalar_coeff:
+        #    assert self.vdim_te == self.vdim_tr, "scalar coefficeint allows only for square matrix"
 
         #print(self.es_weight, self.esflag2, self.esflag)
         for i in range(self.ir.GetNPoints()):
@@ -148,7 +148,10 @@ class PyVectorCurlIntegrator(PyVectorCurlIntegratorBase):
                 tr_merged_arr[:, k] = tr_shape_arr*w2 * \
                     self.es_weight[i]  # nd vdim(d/dx)
 
-            if scalar_coeff:
+            shape = (self.vdim_te, self.vdim_tr)
+            lam = self.eval_complex_lam(trans, ip, shape)
+
+            '''
                 lam = self.lam_real.Eval(trans, ip)
                 if self.lam_imag is not None:
                     lam = lam + 1j*self.lam_imag.Eval(trans, ip)
@@ -159,8 +162,9 @@ class PyVectorCurlIntegrator(PyVectorCurlIntegratorBase):
                 if self.lam_imag is not None:
                     self.lam_imag.Eval(self.vali, trans, ip)
                 lam = lam + 1j*self.vali.GetDataArray()
+            lam = lam.reshape
+            '''
 
-            lam = lam.reshape(self.vdim_te, self.vdim_tr)
             # il + lkj (or lkq)-> ikj (or ikq)
             tmp = np.tensordot(lam, levi_civita3, (1, 0))
 
@@ -243,16 +247,15 @@ class PyVectorDirectionalCurlIntegrator(PyVectorCurlIntegratorBase):
         self.tr_dshape.SetSize(tr_nd, dim)
         self.tr_dshapedxt.SetSize(tr_nd, sdim)
 
-
         tr_shape_arr = self.tr_shape.GetDataArray()
         te_shape_arr = self.te_shape.GetDataArray()
         tr_dshapedxt_arr = self.tr_dshapedxt.GetDataArray()
 
         tr_merged_arr = np.zeros((tr_nd, self.esdim), dtype=np.complex128)
 
-        scalar_coeff = isinstance(self.lam_real, mfem.Coefficient)
-        if scalar_coeff:
-            assert self.vdim_te == self.vdim_tr, "scalar coefficeint allows only for square matrix"
+        #scalar_coeff = isinstance(self.lam_real, mfem.Coefficient)
+        # if scalar_coeff:
+        #    assert self.vdim_te == self.vdim_tr, "scalar coefficeint allows only for square matrix"
 
         #print(self.es_weight, self.esflag2, self.esflag)
         for i in range(self.ir.GetNPoints()):
@@ -279,6 +282,10 @@ class PyVectorDirectionalCurlIntegrator(PyVectorCurlIntegratorBase):
                 tr_merged_arr[:, k] = tr_shape_arr*w2 * \
                     self.es_weight[i]  # nd vdim(d/dx)
 
+            shape = (self.vdim_te, self.vdim_tr)
+            lam = self.eval_complex_lam(trans, ip, shape)
+
+            '''
             if scalar_coeff:
                 lam = self.lam_real.Eval(trans, ip)
                 if self.lam_imag is not None:
@@ -292,6 +299,7 @@ class PyVectorDirectionalCurlIntegrator(PyVectorCurlIntegratorBase):
                 lam = lam + 1j*self.vali.GetDataArray()
 
             lam = lam.reshape(self.vdim_te, self.vdim_tr)
+            '''
 
             # ilj (or ilm)+ lk -> ijk (or imk)
             tmp = np.tensordot(levi_civita3, lam, (1, 0))
@@ -350,5 +358,3 @@ class PyVectorDirectionalCurlIntegrator(PyVectorCurlIntegratorBase):
                         partelmat_arr[:, :] += (M[i, j]
                                                 * vu[:, :]).imag
                         elmat.AddMatrix(self.partelmat, te_nd*i, tr_nd*j)
-
-

petram/helper/integrators/pyvector_diffusion_integrator.py
@@ -196,13 +196,8 @@ class PyVectorDiffusionIntegrator(PyVectorIntegratorBase):
                 vu = np.tensordot(
                     te_shape_arr*w2, tr_shape_arr*w2, 0)*ip.weight  # nd, nd
 
-            self.lam_real.Eval(self.valr, trans, ip)
-            lam = self.valr.GetDataArray()
-            if self.lam_imag is not None:
-                self.lam_imag.Eval(self.vali, trans, ip)
-                lam = lam + 1j*self.vali.GetDataArray()
-            lam = lam.reshape(self.esdim, self.vdim_te,
-                              self.esdim, self.vdim_tr)
+            shape = (self.esdim, self.vdim_te, self.esdim, self.vdim_tr)
+            lam =self.eval_complex_lam(trans, ip, shape)
 
             # lam = [l, i, k, j]
             if self._metric is not None:

petram/helper/integrators/pyvector_hessian_integrator.py
@@ -125,8 +125,8 @@ class PyVectorHessianIntegrator(PyVectorIntegratorBase):
         sdim = trans.GetSpaceDim()
         square = (dim == sdim)
 
-        assert dim ==  sdim, "dim must be sdim"
-        
+        assert dim == sdim, "dim must be sdim"
+
         self.tr_shape.SetSize(tr_nd)
         self.te_shape.SetSize(te_nd)
         self.tr_dshape.SetSize(tr_nd, dim)
@@ -173,11 +173,11 @@ class PyVectorHessianIntegrator(PyVectorIntegratorBase):
             for ll, i in enumerate(self.esflag):
                 for kk, j in enumerate(self.esflag2):
                     trh_merged_arr[:, i, j] = tr_dshape_arr[:,
-                                                               ll]*self.es_weight[kk]
+                                                            ll]*self.es_weight[kk]
             for kk, i in enumerate(self.esflag2):
                 for ll, j in enumerate(self.esflag):
                     trh_merged_arr[:, i, j] = tr_dshape_arr[:,
-                                                               ll]*self.es_weight[kk]
+                                                            ll]*self.es_weight[kk]
             for kk, i in enumerate(self.esflag2):
                 for ll, j in enumerate(self.esflag2):
                     trh_merged_arr[:, i, j] = tr_shape_arr * \
@@ -189,15 +189,8 @@ class PyVectorHessianIntegrator(PyVectorIntegratorBase):
             dudxdvdx = np.tensordot(
                 te_shape_arr, trh_merged_arr, 0)*weight*detJ
 
-            self.lam_real.Eval(self.valr, trans, ip)
-            lam = self.valr.GetDataArray()
-
-            if self.lam_imag is not None:
-                self.lam_imag.Eval(self.vali, trans, ip)
-                lam = lam + 1j*self.vali.GetDataArray()
-
-            lam = lam.reshape(self.vdim_te, self.esdim,
-                              self.esdim, self.vdim_tr)
+            shape = (self.vdim_te, self.esdim, self.esdim, self.vdim_tr)
+            lam = self.eval_complex_lam(trans, ip, shape)
 
             if self._metric:
                 detm = self.eval_sqrtg(trans, ip)
@@ -308,3 +301,32 @@ class PyVectorHessianIntegrator(PyVectorIntegratorBase):
 
 # alias for backword compatibility
 PyVectorPartialPartialIntegrator = PyVectorHessianIntegrator
+
+
+class PyVectorStrongCurlCurlIntegrator(PyVectorHessianIntegrator):
+    def eval_complex_lam(self, trans, ip, shape):
+        shape = (3, 3)
+        lam = PyVectorHessianIntegrator.eval_complex_lam(
+            self, trans, ip, shape)
+
+        if self._metric is not None:
+            lam /= self.eval_sqrtg(trans, ip)   # x /sqrt(g)
+            lam /= self.eval_sqrtg(trans, ip)   # x /sqrt(g)
+
+        # ijk, mk -> ijm
+        tmp = np.tensordot(levi_civita3, lam, (2, 1))
+
+        # ri, ijm -> rjm
+        tmp = np.tensordot(lam, tmp, (1, 0))
+
+        # pqr rjm -. pqjm
+        tmp = np.tensordot(levi_civita3, tmp, (2, 0))
+
+        if self._metric is not None:
+            g_lp = self.eval_cometric(trans, ip)  # x g_{lp}
+
+            # lp, pqjm -> lqjm
+            tmp = np.tensordot(g_lp, tmp, (1, 0))
+
+        # tmp has iklj index
+        return tmp