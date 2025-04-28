f1237c18962d85fde098163490bbfaf40119e795
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2025-04-28 17:35:34 -0400

    fixed typo

--- petram/phys/weakform.py
+++ petram/phys/weakform.py
@@ -202,7 +202,7 @@ class WeakIntegration(Phys):
                             itg_params=itg_params)
 
     def add_bf_contribution(self, engine, a, real=True, kfes=0):
-        if self.get_root_phys().element.startswith("L2") and isinstance(self, Domain):
+        if self.get_root_phys().element.startswith("L2") and not isinstance(self, Domain):
             assert False, "L2 element can have only domain contributions"
         self.add_contribution(engine, a, real=real)
 
