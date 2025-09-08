2ae7a89e120f2417ea150c6ac637bd1c34935106
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2025-09-08 16:22:30 -0400

    removed two comments

--- petram/pi/dlg_plot_sol.py
+++ petram/pi/dlg_plot_sol.py
@@ -988,14 +988,15 @@ class DlgPlotSol(SimpleFramePlus):
         #evt.pp_method = (func, args, kwargs)
         #print("posting event", func)
         #wx.PostEvent(self, evt)
-        print("Using CallAfter")
+
+        #print("Using CallAfter")
         self._plot_data = (func, args, kwargs)
         wx.CallAfter(self.call_plot)
-        #wx.CallAfter(func, *args, **kwargs)
+        # wx.CallAfter(func, *args, **kwargs)
         # wx.CallAfter(self.set_title_no_status)
 
     def call_plot(self):
-        print("thread here", threading.current_thread())
+        # print("thread here", threading.current_thread())
         if self._plot_data is None:
             return
 
