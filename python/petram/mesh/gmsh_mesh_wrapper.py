e9cdd21e771ad38106a5509d06d7b6b50fecc292
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2026-06-04 10:27:52 -0400

    fixed left-over is1d in gmsh_mesh_wrapper

--- python/petram/mesh/gmsh_mesh_wrapper.py
+++ python/petram/mesh/gmsh_mesh_wrapper.py
@@ -1006,7 +1006,7 @@ class GMSHMeshWrapper():
         target = [tag for dim, tag in dimtags if dim == x]
         cp = [tag for dim, tag in self._compound_dimtags if dim == x]
 
-        if np.any(np.in1d(target, cp)):
+        if np.any(np.isin(target, cp)):
             gmsh.option.setNumber("Mesh.MaxNumThreads1D", 1)
             gmsh.option.setNumber("Mesh.MaxNumThreads2D", 1)
             gmsh.option.setNumber("Mesh.MaxNumThreads3D", 1)
